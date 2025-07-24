358ff5ff405ea4d8d6e1b0368d9e3a477ae7518b
ttviet2805 <ttviet2805@gmail.com>
2025-07-24 19:20:21 +0000

    Adding stable diffusion

--- code/models/IBSN.py
+++ code/models/IBSN.py
@@ -473,7 +473,12 @@ class Model_VSN(BaseModel):
                     i = image_id + 1
                     mask_path = "../dataset/valAGE-Set-Mask/" + str(i).zfill(4) + ".png"
                     mask_image = load_image(mask_path)
-                    mask_image = mask_image.resize((512, 512))
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    mask_image = mask_image.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # mask_image = mask_image.resize((512, 512))
+                    # ----- VN END -----
                     image_init = image_batch[j, :, :, :]
                     image_init1 = Image.fromarray((image_init * 255).astype(np.uint8), mode = "RGB")
                     image_mask = np.array(mask_image.convert("L")).astype(np.float32) / 255.0
@@ -517,15 +522,33 @@ class Model_VSN(BaseModel):
                     i = image_id + 1
                     masksrc = "../dataset/valAGE-Set-Mask/"
                     mask_image = load_image(masksrc + str(i).zfill(4) + ".png").convert("RGB")
-                    mask_image = mask_image.resize((512, 512))
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    mask_image = mask_image.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # mask_image = mask_image.resize((512, 512))
+                    # ----- VN END -----
                     h, w = mask_image.size
                     
                     image = image_batch[j, :, :, :]
                     image_init = Image.fromarray((image * 255).astype(np.uint8), mode = "RGB")
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
                     image_inpaint = self.pipe_sdxl(
-                        prompt=prompt, image=image_init, mask_image=mask_image, num_inference_steps=50, strength=0.80, target_size=(512, 512)
+                        prompt=prompt, image=image_init, mask_image=mask_image, num_inference_steps=50, strength=0.80, target_size=(image_size, image_size)
                     ).images[0]
-                    image_inpaint = image_inpaint.resize((512, 512))
+                    # ----- ORIGINAL -----
+                    # image_inpaint = self.pipe_sdxl(
+                    #     prompt=prompt, image=image_init, mask_image=mask_image, num_inference_steps=50, strength=0.80, target_size=(512, 512)
+                    # ).images[0]
+                    # ----- VN END -----
+                    
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    image_inpaint = image_inpaint.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # image_inpaint = image_inpaint.resize((512, 512))
+                    # ----- VN END -----
                     image_inpaint = np.array(image_inpaint) / 255.
                     mask_image = np.array(mask_image) / 255.
                     mask_image = mask_image.astype(np.uint8)
@@ -548,11 +571,22 @@ class Model_VSN(BaseModel):
                     i = image_id + 1
                     masksrc = "../dataset/valAGE-Set-Mask/" + str(i).zfill(4) + ".png"
                     mask_image = Image.open(masksrc).convert("RGB")
-                    mask_image = mask_image.resize((256, 256))
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    mask_image = mask_image.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # mask_image = mask_image.resize((512, 512))
+                    # ----- VN END -----
                     mask_image = Image.fromarray(255 - np.array(mask_image))
                     image = image_batch[j, :, :, :]
                     original_image = Image.fromarray((image * 255).astype(np.uint8), mode = "RGB")
-                    original_image = original_image.resize((256, 256))
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    original_image = original_image.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # original_image = original_image.resize((256, 256))
+                    # ----- VN END -----
+                    
                     output = self.pipe_repaint(
                         image=original_image,
                         mask_image=mask_image,
@@ -563,9 +597,19 @@ class Model_VSN(BaseModel):
                         generator=generator,
                     )
                     image_inpaint = output.images[0]
-                    image_inpaint = image_inpaint.resize((512, 512))
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    image_inpaint = image_inpaint.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # image_inpaint = image_inpaint.resize((512, 512))
+                    # ----- VN END -----
                     image_inpaint = np.array(image_inpaint) / 255.
-                    mask_image = mask_image.resize((512, 512))
+                    # ----- VN START -----
+                    image_size = global_variables.TEST_CONFIG['datasets']['TD']['image_size']
+                    mask_image = mask_image.resize((image_size, image_size))
+                    # ----- ORIGINAL -----
+                    # mask_image = mask_image.resize((512, 512))
+                    # ----- VN END -----
                     mask_image = np.array(mask_image) / 255.
                     mask_image = mask_image.astype(np.uint8)
                     image_fuse = image * mask_image + image_inpaint * (1 - mask_image)
